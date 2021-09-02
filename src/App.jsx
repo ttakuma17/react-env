2ce8b94be31c8f472cfb6e26693236198449cfa8
ttakuma17 <tk21.dek93k@gmail.com>
2021-09-02 22:04:10 +0900

    set ESLint and Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import React from 'react';
+import { react } from '@babel/types';// eslint-disable-line
 import logo from './logo.svg';
 import './App.css';
 
+const a = 1;
+// console.log(b);
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
